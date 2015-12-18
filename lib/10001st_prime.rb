c026d587e7811388747efa2348bba318c80f9647
dougtebay <dougtebay@gmail.com>
2015-12-17 20:13:02 -0500

    All tests passing

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,15 @@
 # Implement your procedural solution here!
+def prime_number_for(num)
+  counter = 1
+  primes = 0
+  while primes < num
+    counter += 1
+    primes += 1 if is_prime?(counter)
+  end
+  counter
+end
+
+def is_prime?(num)
+  (Math.sqrt(num).to_i).downto(2) { |i| return false if num % i == 0 }
+  return true
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,21 @@
 # Implement your object-oriented solution here!
+class Prime
+  def initialize(num)
+    @counter = 1
+    primes = 0
+    while primes < num
+      @counter += 1
+      primes += 1 if is_prime?(@counter)
+    end
+    @counter
+  end
+
+  def is_prime?(num)
+    (Math.sqrt(num).to_i).downto(2) { |i| return false if num % i == 0 }
+    return true
+  end
+
+  def number
+    @counter
+  end
+end
